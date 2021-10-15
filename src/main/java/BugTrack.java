c8678e826f9462e6c49537efee58d5940ba92162
Vasilii Lokky <45848432+Lokky27@users.noreply.github.com>
2021-10-15 17:18:29 +0300

    Add user input, Add logging

---
+++ src/main/java/BugTrack.java
@@ -0,0 +1,17 @@
+import models.Priority;
+import models.Project;
+import models.Task;
+import models.User;
+import service.TaskService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class BugTrack
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+}
